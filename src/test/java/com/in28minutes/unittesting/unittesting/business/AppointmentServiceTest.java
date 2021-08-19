a0e15a4006dbee1e77e3af67e4db4098e718aa9d
devenshettyss <practice.devenshetty@gmail.com>
2021-08-19 12:16:04 +0000

    CRUD operations

--- src/test/java/com/in28minutes/unittesting/unittesting/business/AppointmentServiceTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/AppointmentServiceTest.java
@@ -1,14 +1,18 @@
 package com.in28minutes.unittesting.unittesting.business;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import com.in28minutes.unittesting.unittesting.data.AppointmentRepository;
 import com.in28minutes.unittesting.unittesting.data.ItemRepository;
 import com.in28minutes.unittesting.unittesting.model.Appointment;
@@ -23,12 +27,14 @@ class AppointmentServiceTest {
 	@InjectMocks
 	AppointmentServices appointments;
 
+	//read 
 	@Test
 	void testRetrieveAllAppointmrnts() {
-		when(repo.findAll()).thenReturn(Arrays.asList(new Appointment(1,"Ram","something@gmail.com","chennai",1234567890L,120f)));
+		when(repo.findAll()).thenReturn(Arrays.asList(new Appointment(1,"hu","h@gmail.com","mumbai",1234567890L,120f)));
 		appointments.retrieveAllAppointments();
 	}
 	
+	//create
 	void testSavedAppointments() {
 		Appointment  a1= new Appointment(2,"mu","m@gmail.com","delhi",1234567890L,67f);
 		when(repo.save(a1)).thenReturn(a1);
@@ -36,12 +42,21 @@ class AppointmentServiceTest {
 		assertEquals(a1.getId(), saved.getId());
 	}
 	
+	//update
 	void testUpdateAppoinments() {
 		Optional<Appointment> a1 = Optional.of(new Appointment(2,"mu","m@gmail.com","delhi",1234567890L,67f));
 		when(repo.findById(2)).thenReturn(a1);
-		Appointment app=appointments.updateItem(2);
-		assertNotEquals(a1.get().getId(), app.getId());
+		Appointment app=appointments.updateAppointment(2);
+		assertEquals(a1.get().getId(), app.getId());
+		assertNotEquals(a1.get().getNumber(),app.getNumber());
 	}
 	
+	//delete
+	void testDeleteAppointment() {
+		Optional<Appointment> a1 = Optional.of(new Appointment(2,"mu","m@gmail.com","delhi",1234567890L,67f));
+		when(repo.findById(2)).thenReturn(a1).thenReturn(null);
+		Appointment app= appointments.deleteAppointment(2);
+		assertNull(app);
+	}
 	
 }
